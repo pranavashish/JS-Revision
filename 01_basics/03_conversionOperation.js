c59ee405b6e0e9600eb16e3726347cb5924219a0
pranavashish <pranavashish@gmail.com>
2023-11-26 13:13:18 +0530

    03

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33CC";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33CC" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => true
+// Pranav => true
+// null, undefined => false
+
+let someNum = 33;
+
+let stringNumber = String(someNum);
+console.log(stringNumber);
+console.log(typeof stringNumber);
