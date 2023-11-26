08af9dbf35cc1b263c27a21a6880f3c0d62884ed
pranavashish <pranavashish@gmail.com>
2023-11-27 00:12:34 +0530

    dataTypes :3

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,31 @@
 let someNum = 33;
 
 let stringNumber = String(someNum);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************************** Operrations **********************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// 2**3 matlab 2 power 3
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2);
+// console.log(1 + 2);
+
+//  console.log(+true);
+//  console.log(++true);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
